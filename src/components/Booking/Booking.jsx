7e27f579243008847e858dd6a91359555b2748c6
Lalitha Kunchala <lalithakunchala@gmail.com>
2020-09-01 18:30:30 +0530

    user profile display

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
+import styles from './Booking.module.css'
 
 export class Booking extends Component {
     constructor(props){
@@ -14,14 +15,14 @@ export class Booking extends Component {
             const productObj = this.props.users.find((element) => element.id == this.props.id) 
         return (
             <div>
-                <Link to="/"><div>Home</div></Link>
+                <Link to="/"><div style={{float:"right",padding:"20px"}}>Home</div></Link>
                      
-                {productObj && <div style = {{border : "1px solid black"}}>
-                    <p> {productObj.first_name} </p>
-                    <p> {productObj.last_name} </p>
+                {productObj && <div className={styles.mainDiv}>
+                    <div><img  src={productObj.avatar} alt=""/></div>
+                    <div className={styles.nameDiv}>Firstname: <span className={styles.nam}>{productObj.first_name}</span></div>
+                    <div className={styles.nameDiv}>LastName: {productObj.last_name} </div>
+                    <div className={styles.nameDiv}>Email: {productObj.email} </div>
                 </div> }
-        
-    }
             </div>
             )
     }
